4dabcab79f92d1592385f313b1c1fa9d831fb23e
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-05 17:42:40 -0400

    Agregando Ejercicio 4 Capt 6

--- EjerciciosTarea3/Ejercicios/Capt6/Ejercicio6_4.xaml.cs
+++ EjerciciosTarea3/Ejercicios/Capt6/Ejercicio6_4.xaml.cs
@@ -5,9 +5,56 @@ using System;
 namespace Tarea3.Ejercicios.Capt6
 {
     public partial class Ejercicio6_4 : Window{
+        private float aula;
+        private float[][]? notas;
+        private float cantidad_Estudiantes;
+        private float contador;
+        private float suma;
         public Ejercicio6_4()
         {
             InitializeComponent();
+        }
+        private void AgregarBTN_Click(object sander, RoutedEventArgs e)
+        {
+            aula = float.Parse(CantidadCursosTextBox.Text);
+            notas = new float[(int)aula][];
+            for (int i = 0; i < aula; i++)
+            {
+                cantidad_Estudiantes = float.Parse(Microsoft.VisualBasic.Interaction.InputBox($"Digite la cantidad de alumnos en el salon #{i + 1}", " Cantidad de alumnos", " ", 0));
+                notas[i] = new float[(int)cantidad_Estudiantes];
+                contador += cantidad_Estudiantes;
+                cantidad_Estudiantes += cantidad_Estudiantes;
+            }
+        }
+        private void AgregarNotasBTN_Click(object sander, RoutedEventArgs e)
+        {
+            for (int i = 0; i < aula; i++)
+            {
+                for (int m = 0; m < notas[i].Length; m++)
+                {
+                    notas[i][m] = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox($"Digite la calificación del alumno # {m + 1}  del salón {i + 1}", "Calificaciones", " ", 0));
+                    suma += notas[i][m];
+                }
+            }
+        }
+        private void ResultadoBTN_Click(object sander, RoutedEventArgs e)
+        {
+            float promedio = calcularPromedio(notas);
+            MessageBox.Show($"El promedio calculado fue {promedio}", "Promedio");
+        }
+        static float calcularPromedio(float[][] arreglo)
+        {
+            float nota =0f;
+            int estudiantes = 0;
+            for(int i=0;i<arreglo.Length;i++)
+            {
+                for(int j=0;j<arreglo[i].Length;j++)
+                {
+                    nota+=arreglo[i][j];
+                    estudiantes++;
+                }
+            }
+            return nota/estudiantes;
         }
     }
 }
