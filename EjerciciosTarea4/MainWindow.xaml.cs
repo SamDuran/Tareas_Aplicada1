1f6da824b4a6b59406f66f9c94a6e0b6fc1b2f7c
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-12 19:32:46 -0400

    Agregando Tarea 4

---
+++ EjerciciosTarea4/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+/* using EjerciciosTarea4.UI.Capt8; */
+/* using EjerciciosTarea4.UI.Capt9; */
+
+namespace EjerciciosTarea4
+{
+    /// <summary>
+    /// Pantalla principal
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void menuEjercicio8_3_Click(object sander, RoutedEventArgs e)
+        {
+            
+        }
+        private void menuEjercicio8_5_Click(object sander, RoutedEventArgs e)
+        {
+
+        }
+        private void menuEjercicio9_1_Click(object sander, RoutedEventArgs e)
+        {
+
+        }
+        private void menuEjercicio9_3_Click(object sander, RoutedEventArgs e)
+        {
+
+        }
+        private void menuEjercicio9_4_Click(object sander, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
