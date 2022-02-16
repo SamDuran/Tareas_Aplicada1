5ade7ebffa5141f07559c5911aa60ee8f7407956
SamDuran <samidt0186@gmail.com>
2022-02-15 22:29:19 -0400

    Ejemplo Blazor incompleto

---
+++ EjemploBlazor/Migrations/20220215234507_inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Existencia = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ValorInventario = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
